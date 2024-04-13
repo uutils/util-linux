294c6b35b32626bcf9967baccb0ca6acae80a295
sisungo <sisungo@icloud.com>
2024-04-14 07:14:07 +0800

    Add tests for `ctrlaltdel`

---
+++ tests/by-util/test_ctrlaltdel.rs
@@ -0,0 +1,19 @@
+// This file is part of the uutils util-linux package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+// spell-checker:ignore (words) symdir somefakedir
+
+use std::path::PathBuf;
+
+use crate::common::util::{TestScenario, UCommand};
+
+#[test]
+fn test_invalid_arg() {
+    new_ucmd!().arg("--definitely-invalid").fails().code_is(1);
+}
+
+#[test]
+fn test_ctrlaltdel() {
+    new_ucmd!().succeeds();
+}
