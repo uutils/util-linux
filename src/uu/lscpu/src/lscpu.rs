fc7bf39d1b791fc12ade0fcf5678af0a77d52977
Yang Hau <yuanyanghau@gmail.com>
2024-07-25 04:54:17 +0800

    lscpu: Add option --json
    closes #15

--- src/uu/lscpu/src/lscpu.rs
+++ src/uu/lscpu/src/lscpu.rs
@@ -3,31 +3,77 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-use clap::{crate_version, Command};
+use clap::{crate_version, Arg, ArgAction, Command};
 use regex::Regex;
-use std::fs;
+use serde::Serialize;
+use std::{fs, str::FromStr};
 use sysinfo::System;
 use uucore::{error::UResult, format_usage, help_about, help_usage};
+
+mod options {
+    pub const JSON: &str = "json";
+}
 
 const ABOUT: &str = help_about!("lscpu.md");
 const USAGE: &str = help_usage!("lscpu.md");
 
+#[derive(Serialize)]
+struct CpuInfos {
+    lscpu: Vec<CpuInfo>,
+}
+
+#[derive(Serialize)]
+struct CpuInfo {
+    field: String,
+    data: String,
+}
+
+impl CpuInfos {
+    fn new() -> CpuInfos {
+        CpuInfos {
+            lscpu: Vec::<CpuInfo>::new(),
+        }
+    }
+    fn push(&mut self, field: &str, data: &str) {
+        let cpu_info = CpuInfo {
+            field: String::from_str(field).unwrap(),
+            data: String::from_str(data).unwrap(),
+        };
+        self.lscpu.push(cpu_info);
+    }
+    fn to_json(&self) -> String {
+        serde_json::to_string_pretty(self).unwrap()
+    }
+}
+
 #[uucore::main]
 pub fn uumain(args: impl uucore::Args) -> UResult<()> {
-    let _matches: clap::ArgMatches = uu_app().try_get_matches_from(args)?;
+    let matches: clap::ArgMatches = uu_app().try_get_matches_from(args)?;
+
+    let json = matches.get_flag(options::JSON);
+
     let system = System::new_all();
     let _cpu = system.global_cpu_info();
 
-    println!("Architecture: {}", get_architecture());
-    println!("CPU(s): {}", system.cpus().len());
+    let mut cpu_infos = CpuInfos::new();
+    cpu_infos.push("Architecture", &get_architecture());
+    cpu_infos.push("CPU(s)", &format!("{}", system.cpus().len()));
     // Add more CPU information here...
 
     if let Ok(contents) = fs::read_to_string("/proc/cpuinfo") {
         let re = Regex::new(r"^model name\s+:\s+(.*)$").unwrap();
         // Assuming all CPUs have the same model name
         if let Some(cap) = re.captures_iter(&contents).next() {
-            println!("Model name: {}", &cap[1]);
+            cpu_infos.push("Model name", &cap[1]);
         };
+    }
+
+    if json {
+        println!("{}", cpu_infos.to_json());
+    } else {
+        for elt in cpu_infos.lscpu {
+            println!("{}: {}", elt.field, elt.data);
+        }
     }
     Ok(())
 }
@@ -48,4 +94,10 @@ pub fn uu_app() -> Command {
         .about(ABOUT)
         .override_usage(format_usage(USAGE))
         .infer_long_args(true)
+        .arg(
+            Arg::new(options::JSON)
+                .long("json")
+                .help("Use JSON output format for the default summary or extended output (see --extended). For backward compatibility, JSON output follows the default summary behavior for non-terminals (e.g., pipes) where subsections are missing. See also --hierarchic.")
+                .action(ArgAction::SetTrue),
+        )
 }
--- tests/by-util/test_lscpu.rs
+++ tests/by-util/test_lscpu.rs
@@ -4,9 +4,18 @@
 // file that was distributed with this source code.
 
 use crate::common::util::TestScenario;
+use serde_json::{self, Value};
 
 #[test]
 #[ignore]
 fn test_invalid_arg() {
     new_ucmd!().arg("--definitely-invalid").fails().code_is(1);
+}
+
+#[test]
+fn test_json() {
+    let result = new_ucmd!().arg("--json").succeeds();
+    let stdout_bytes = result.stdout();
+    let res: Result<Value, _> = serde_json::from_slice(&stdout_bytes);
+    assert!(res.is_ok(), "invalid json output");
 }
