565f65c787abeeebb21510ef0a8079bfaa8e5650
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2024-07-27 15:43:01 +0200

    rev: add some tests

--- tests/by-util/test_rev.rs
+++ tests/by-util/test_rev.rs
@@ -9,3 +9,62 @@ use crate::common::util::TestScenario;
 fn test_invalid_arg() {
     new_ucmd!().arg("--definitely-invalid").fails().code_is(1);
 }
+
+#[test]
+fn test_piped_in_data() {
+    new_ucmd!().pipe_in("a test").succeeds().stdout_is("tset a");
+}
+
+#[test]
+fn test_existing_file() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.write("a.txt", "line A\nline B");
+
+    ucmd.arg("a.txt").succeeds().stdout_is("A enil\nB enil");
+}
+
+#[test]
+fn test_multiple_files() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.write("a.txt", "file A\n");
+    at.write("b.txt", "file B\n");
+
+    ucmd.args(&["a.txt", "b.txt"])
+        .succeeds()
+        .stdout_is("A elif\nB elif\n");
+}
+
+#[test]
+fn test_empty_file() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.touch("empty.txt");
+
+    ucmd.arg("empty.txt").succeeds().no_output();
+}
+
+#[test]
+fn test_non_existing_file() {
+    new_ucmd!()
+        .arg("non_existing_file")
+        .fails()
+        .code_is(1)
+        .no_stdout()
+        .stderr_contains("cannot open non_existing_file: No such file or directory");
+}
+
+#[test]
+fn test_non_existing_and_existing_file() {
+    let (at, mut ucmd) = at_and_ucmd!();
+
+    at.write("a.txt", "file A");
+
+    ucmd.arg("non_existing_file")
+        .arg("a.txt")
+        .fails()
+        .code_is(1)
+        .stderr_contains("cannot open non_existing_file: No such file or directory")
+        .stdout_is("A elif");
+}
