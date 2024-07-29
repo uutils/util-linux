afd6056033e957d4d984219f39dc926afc605752
Andy Xie <andy.xie.opensource@gmail.com>
2024-07-29 21:20:31 +0800

    Added test_short_invalid_utmp_file

--- tests/by-util/test_last.rs
+++ tests/by-util/test_last.rs
@@ -7,6 +7,8 @@
 use crate::common::util::TestScenario;
 
 use regex::Regex;
+use std::fs;
+use std::io::Write;
 
 #[test]
 #[cfg(unix)]
@@ -27,7 +29,7 @@ fn test_last() {
 #[test]
 #[cfg(unix)]
 fn test_limit_arg() {
-    let line_check = |input: &str| input.lines().count() == 1;
+    let line_check = |input: &str| input.lines().count() == 3;
     new_ucmd!()
         .arg("--limit=1")
         .succeeds()
@@ -83,6 +85,24 @@ fn test_timestamp_format_iso() {
             .unwrap();
     new_ucmd!()
         .arg("--time-format=iso")
+        .succeeds()
+        .stdout_matches(&regex);
+}
+
+#[test]
+#[cfg(unix)]
+fn test_short_invalid_utmp_file() {
+    let filepath = "/tmp/testfile";
+    let testfile = fs::File::create(filepath);
+    // Random bytes
+    let data: Vec<u8> = vec![4, 5, 6, 16, 8, 13, 2, 12, 5, 3, 11, 5, 1, 13, 1, 1, 0, 9, 5, 5, 2, 8, 4];
+    let _ = testfile.unwrap().write_all(&data);
+
+    let regex = Regex::new(r"\n\S*\sbegins\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s*(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s*[0-9][0-9]?\s*[0-9][0-9]:[0-9][0-9]")
+            .unwrap();
+
+    new_ucmd!()
+        .arg(format!("--file={filepath}"))
         .succeeds()
         .stdout_matches(&regex);
 }
