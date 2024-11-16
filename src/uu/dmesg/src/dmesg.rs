e321aeefc3e4f719d67694edf0047fbc6b0ad723
Fuad Ismail <fuad1502@gmail.com>
2024-11-16 23:43:06 +0700

    dmesg: add license header.

--- src/uu/dmesg/src/dmesg.rs
+++ src/uu/dmesg/src/dmesg.rs
@@ -1,3 +1,8 @@
+// This file is part of the uutils util-linux package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 use clap::{crate_version, Arg, ArgAction, Command};
 use regex::Regex;
 use std::fs;
