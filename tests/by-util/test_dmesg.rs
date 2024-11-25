4c1812f128049781442ff0f130994af43436ffaa
Fuad Ismail <fuad1502@gmail.com>
2024-11-25 13:53:12 +0700

    tests/dmesg: separate time format tests.

--- tests/by-util/test_dmesg.rs
+++ tests/by-util/test_dmesg.rs
@@ -32,19 +32,45 @@ fn test_kmsg_json() {
 }
 
 #[test]
-fn test_kmsg_time_format() {
-    let time_formats = ["delta", "reltime", "ctime", "notime", "iso", "raw"];
-    for format in time_formats {
-        let time_format_arg = format!("--time-format={format}");
-        let expected_output = format!("test_kmsg_time_format_{format}.expected");
-        new_ucmd!()
-            .arg("--kmsg-file")
-            .arg("kmsg.input.1")
-            .arg(time_format_arg)
-            .succeeds()
-            .no_stderr()
-            .stdout_is_fixture(expected_output);
-    }
+fn test_kmsg_time_format_delta() {
+    test_kmsg_time_format("delta");
+}
+
+#[test]
+fn test_kmsg_time_format_reltime() {
+    test_kmsg_time_format("reltime");
+}
+
+#[test]
+fn test_kmsg_time_format_ctime() {
+    test_kmsg_time_format("ctime");
+}
+
+#[test]
+fn test_kmsg_time_format_notime() {
+    test_kmsg_time_format("notime");
+}
+
+#[test]
+fn test_kmsg_time_format_iso() {
+    test_kmsg_time_format("iso");
+}
+
+#[test]
+fn test_kmsg_time_format_raw() {
+    test_kmsg_time_format("raw");
+}
+
+fn test_kmsg_time_format(format: &str) {
+    let time_format_arg = format!("--time-format={format}");
+    let expected_output = format!("test_kmsg_time_format_{format}.expected");
+    new_ucmd!()
+        .arg("--kmsg-file")
+        .arg("kmsg.input.1")
+        .arg(time_format_arg)
+        .succeeds()
+        .no_stderr()
+        .stdout_is_fixture(expected_output);
 }
 
 #[test]
