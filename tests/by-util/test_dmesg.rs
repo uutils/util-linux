d5b2f3c7e157ddbc89fc3873aed0e622bd8ab400
Fuad Ismail <fuad1502@gmail.com>
2024-12-14 22:27:54 +0700

    tests/dmesg: remove enclosing quotes surrounding since and until option arguments.

--- tests/by-util/test_dmesg.rs
+++ tests/by-util/test_dmesg.rs
@@ -170,8 +170,8 @@ fn test_since_until() {
     new_ucmd!()
         .arg("--kmsg-file")
         .arg("kmsg.input")
-        .arg("--since=\"2024-11-19 17:47:32 +0700\"")
-        .arg("--until='2024-11-19 18:55:52 +0700'")
+        .arg("--since=2024-11-19 17:47:32 +0700")
+        .arg("--until=2024-11-19 18:55:52 +0700")
         .succeeds()
         .no_stderr()
         .stdout_is_templated_fixture("test_since_until.expected", &[("\r\n", "\n")]);
