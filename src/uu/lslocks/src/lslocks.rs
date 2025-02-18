c2571cb6fac365151a48c42342b1baf40c47b1a1
alxndrv <>
2025-02-18 16:18:49 +0200

    `lslocks`: Set up `lslocks` entry point

---
+++ src/uu/lslocks/src/lslocks.rs
@@ -0,0 +1,21 @@
+// This file is part of the uutils util-linux package.
+//
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
+use clap::{crate_version, Command};
+use uucore::error::UResult;
+
+#[uucore::main]
+pub fn uumain(_args: impl uucore::Args) -> UResult<()> {
+    println!("lslocks: Hello world");
+    Ok(())
+}
+
+pub fn uu_app() -> Command {
+    Command::new(uucore::util_name())
+        .version(crate_version!())
+        //.about(ABOUT)
+        //.override_usage(format_usage(USAGE))
+        .infer_long_args(true)
+}
