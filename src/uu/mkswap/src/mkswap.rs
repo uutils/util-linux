2b4f93b766649ccb099954b5c39054d34ca08d50
silverbaum <topias.silfverhuth@gmail.com>
2025-04-19 17:03:13 +0300

    add uuid and label support, separated some functions

--- src/uu/mkswap/src/mkswap.rs
+++ src/uu/mkswap/src/mkswap.rs
@@ -5,16 +5,19 @@
 
 use clap::{crate_version, Arg, ArgAction, ArgMatches, Command};
 use std::{
-    env, fs,
+    env,
+    fs::{self, File, Metadata},
     io::{BufRead, BufReader, Write},
     os::{fd::AsRawFd, linux::fs::MetadataExt},
     path::Path,
+    str::FromStr,
 };
 use uucore::{
     error::{set_exit_code, UResult, USimpleError, UUsageError},
     format_usage, help_about, help_usage,
     libc::{ioctl, sysconf, _IO, _SC_PAGESIZE, _SC_PAGE_SIZE},
 };
+use uuid::Uuid;
 
 const ABOUT: &str = help_about!("mkswap.md");
 const USAGE: &str = help_usage!("mkswap.md");
@@ -32,13 +35,62 @@ struct SwapHeader {
     last_page: u32,
     nr_badpages: u32,
     uuid: [u8; 16],
-    volume_name: [char; 16],
+    volume_name: [u8; 16],
     padding: [u32; 117],
     badpages: [u32; 1],
 }
 
+fn getsize(fd: &File, stat: &Metadata, devname: &str) -> Result<u128, std::io::Error> {
+    let devsize: u128;
+    /* for block devices, ioctl call with manual size reading as a backup method */
+    if stat.st_mode() == 25008 {
+        let mut sectors: u128 = 0;
+        let err = unsafe { ioctl(fd.as_raw_fd(), BLKGETSIZE, &mut sectors) };
+
+        if sectors == 0 || err < 0 {
+            let f_size = fs::File::open(format!("/sys/class/block/{}/size", devname))?;
+
+            let reader = BufReader::new(f_size);
+            let vec: Vec<Result<u128, _>> = reader
+                .lines()
+                .map(|v| v.unwrap().parse::<u128>())
+                .collect::<Vec<Result<u128, _>>>();
+            sectors = vec[0].clone().unwrap_or(0);
+        }
+        devsize = sectors * 512;
+    } else {
+        devsize = stat.st_size() as u128;
+    }
+
+    Ok(devsize)
+}
+
+unsafe fn init_signature_page(pagesize: usize, pages: u128, uuid: Uuid, label: &str) -> Box<[u8]> {
+    let mut buf = Box::<[u8]>::new_uninit_slice(pagesize);
+
+    unsafe {
+        buf.as_mut_ptr().write_bytes(0, pagesize);
+
+        //fill up swap header
+        let swap_hdr = buf.as_mut_ptr() as *mut SwapHeader;
+        (*swap_hdr).version = SWAP_VERSION;
+        (*swap_hdr).last_page = (pages - 1) as u32;
+        if !uuid.is_nil() {
+            (*swap_hdr).uuid = *uuid.as_bytes();
+        }
+        if !label.is_empty() {
+            (*swap_hdr)
+                .volume_name
+                .as_mut_ptr()
+                .copy_from(label.as_ptr(), label.len());
+        }
+        buf.assume_init()
+    }
+}
+
 pub fn mkswap(args: &ArgMatches) -> UResult<()> {
     let devstr;
+
     match args.get_one::<String>("device") {
         Some(str) => devstr = str,
         None => {
@@ -48,6 +100,11 @@ pub fn mkswap(args: &ArgMatches) -> UResult<()> {
             ))
         }
     }
+
+    let label = match args.get_one::<String>("label") {
+        Some(l) => l.as_str(),
+        None => "",
+    };
 
     let dev = Path::new(devstr.as_str());
     let devname = devstr.strip_prefix("/dev/").unwrap_or("err");
@@ -61,6 +118,17 @@ pub fn mkswap(args: &ArgMatches) -> UResult<()> {
 
     let stat = fd.metadata()?;
 
+    let uuid = match args.get_one::<String>("uuid") {
+        Some(str) => Uuid::from_str(str).expect("Unable to parse UUID"),
+        None => {
+            if stat.st_mode() == 25008 {
+                Uuid::new_v4()
+            } else {
+                Uuid::nil()
+            }
+        }
+    };
+
     if stat.st_uid() != 0 {
         println!(
             "{}: {}: insecure file owner {}, fix with: chown 0:0 {}",
@@ -71,27 +139,7 @@ pub fn mkswap(args: &ArgMatches) -> UResult<()> {
         );
     }
 
-    let devsize: u128;
-
-    /* for block devices, ioctl call with manual size reading as a backup method */
-    if stat.st_mode() == 25008 {
-        let mut sectors: u128 = 0;
-        let err = unsafe { ioctl(fd.as_raw_fd(), BLKGETSIZE, &mut sectors) };
-
-        if sectors == 0 || err < 0 {
-            let f_size = fs::File::open(format!("/sys/class/block/{devname}/size"))?;
-
-            let reader = BufReader::new(f_size);
-            let vec: Vec<Result<u128, _>> = reader
-                .lines()
-                .map(|v| v.unwrap().parse::<u128>())
-                .collect::<Vec<Result<u128, _>>>();
-            sectors = vec[0].clone().unwrap_or(0);
-        }
-        devsize = sectors * 512;
-    } else {
-        devsize = stat.st_size() as u128;
-    }
+    let devsize: u128 = getsize(&fd, &stat, devname)?;
 
     let mut pagesize: i64 = stat.st_blksize() as i64;
     if pagesize <= 0 {
@@ -117,21 +165,9 @@ pub fn mkswap(args: &ArgMatches) -> UResult<()> {
     assert!(devsize > 0);
 
     let pages: u128 = devsize / pagesize as u128;
-    let lastpage = pages - 1;
 
     //signature page
-    let mut buf = Box::<[u8]>::new_uninit_slice(pagesize as usize);
-
-    unsafe {
-        buf.as_mut_ptr().write_bytes(0, pagesize as usize);
-
-        //fill up swap header
-        let swap_hdr = buf.as_mut_ptr() as *mut SwapHeader;
-        (*swap_hdr).version = SWAP_VERSION;
-        (*swap_hdr).last_page = lastpage as u32;
-    }
-
-    let mut buf = unsafe { buf.assume_init() };
+    let mut buf = unsafe { init_signature_page(pagesize as usize, pages, uuid, label) };
 
     //write swap signature
     let _ = &buf[(pagesize as usize - SWAP_SIGNATURE_SZ)..pagesize as usize]
@@ -141,10 +177,20 @@ pub fn mkswap(args: &ArgMatches) -> UResult<()> {
     fd.flush()?;
     fd.sync_all()?;
 
-    println!(
-        "Setting up swapspace version 1, size = {}KiB",
-        (((pages - 1) * pagesize as u128) / 1024)
-    );
+    if label.is_empty() {
+        println!(
+            "Setting up swapspace version 1, size = {}KiB\nNo label, UUID={}",
+            (((pages - 1) * pagesize as u128) / 1024),
+            uuid
+        );
+    } else {
+        println!(
+            "Setting up swapspace version 1, size = {}KiB\nLabel={}, UUID={}",
+            (((pages - 1) * pagesize as u128) / 1024),
+            label,
+            uuid
+        )
+    }
 
     Ok(())
 }
@@ -171,5 +217,19 @@ pub fn uu_app() -> Command {
                 .long("device")
                 .action(ArgAction::Set)
                 .help("block device"),
+        )
+        .arg(
+            Arg::new("label")
+                .short('l')
+                .long("label")
+                .action(ArgAction::Set)
+                .help("specify a label"),
+        )
+        .arg(
+            Arg::new("uuid")
+                .short('u')
+                .long("uuid")
+                .action(ArgAction::Set)
+                .help("set a uuid"),
         )
 }
