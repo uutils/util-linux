017ac997530f83f168327116d1a28bde69813b9b
garasubo <garasubo@gmail.com>
2026-04-26 12:26:36 +0900

    compare test results with baseline

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -9,7 +9,18 @@ cargo build --release
 
 ./scripts/gen-test-helper.sh
 
+# Clear stale diff files from any previous run
+rm -rf "$PROJECT_DIR/.test-helpers/tests/diff"
+
+# Run GNU tests; allow non-zero exit (known failures exist)
+set +e
 "$GNU_PROJECT_DIR/tests/run.sh" \
     --builddir="$PROJECT_DIR/.test-helpers" \
     "$@" \
     cal dmesg hexdump lscpu lslocks lsmem
+GNU_TEST_EXIT=$?
+set -e
+
+[[ $GNU_TEST_EXIT -ne 0 ]] && echo "GNU test runner exited with code $GNU_TEST_EXIT"
+
+./scripts/check-new-gnu-failures.sh
