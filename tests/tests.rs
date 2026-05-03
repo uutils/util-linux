745a6533702c33846f2ed0cd35fc37a8d980eb76
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2026-05-03 14:47:12 +0200

    tests.rs: adapt to change in ctor

--- tests/tests.rs
+++ tests/tests.rs
@@ -8,7 +8,7 @@ use std::env;
 pub const TESTS_BINARY: &str = env!("CARGO_BIN_EXE_util-linux");
 
 // Use the ctor attribute to run this function before any tests
-#[ctor::ctor]
+#[ctor::ctor(unsafe)]
 fn init() {
     unsafe {
         // Necessary for uutests to be able to find the binary
